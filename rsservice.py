fa40053d9dd66a16a1919ea37c9868b35a585492
Izak Burger <iburger@victronenergy.com>
2024-11-15 16:53:32 +0200

    change how summaries are done
    Pass the entire leader service object to the summariser rather
    than just the list of subservices. This allows the summariser
    to also gain access to other members, for example settings.

--- rsservice.py
+++ rsservice.py
@@ -8,27 +8,27 @@ class Summary(object):
 		self.make_item = IntegerItem if item is None else item
 		self.path = path
 
-	def summarise(self, services):
+	def summarise(self, leader):
 		raise NotImplementedError("summarise")
 
 class SummaryAll(Summary):
-	def summarise(self, services):
-		return int(all(x.get_value(self.path) for x in services))
+	def summarise(self, leader):
+		return int(all(x.get_value(self.path) for x in leader.subservices))
 
 class SummaryAny(Summary):
-	def summarise(self, services):
-		return int(any(x.get_value(self.path) for x in services))
+	def summarise(self, leader):
+		return int(any(x.get_value(self.path) for x in leader.subservices))
 
 class SummaryMax(Summary):
-	def summarise(self, services):
+	def summarise(self, leader):
 		try:
-			return max(y for y in (x.get_value(self.path) for x in services) if y is not None)
+			return max(y for y in (x.get_value(self.path) for x in leader.subservices) if y is not None)
 		except ValueError:
 			return None
 
 class SummaryFirst(Summary):
-	def summarise(self, services):
-		for x in services:
+	def summarise(self, leader):
+		for x in leader.subservices:
 			return x.get_value(self.path)
 		return None
 
