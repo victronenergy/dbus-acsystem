d5ec35629a4335b6d377be5d897ceb39a0ba4e4e
Dirk-Jan Faber <dfaber@victronenergy.com>
2024-12-27 22:14:51 +0100

    Add DESS capability for HS-19
    Modify the check for Dynamic ESS capability by, apart from looking at the
    firmware version, looking at the product id as well.

--- dbus-acsystem.py
+++ dbus-acsystem.py
@@ -237,7 +237,9 @@ class Service(_Service):
 	def update_capabilities(self):
 		with self as s:
 			s["/Capabilities/HasDynamicEssSupport"] = int(all(
-				(x.firmwareversion or 0) >= 0x11713 for x in self.subservices))
+				((0xA440 <= (x.productid or 0) <= 0xA47F) and (x.firmwareversion or 0) >= 0x11713) or # Multi RS
+				((0xA480 <= (x.productid or 0) <= 0xA4BF) and (x.firmwareversion or 0) >= 0x10043)    # HS-19
+				for x in self.subservices))
 
 	def update_summaries(self):
 		with self as s:
